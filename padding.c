6078c9af9326433b31ecde57a9789f1bbcfd6772
patracheTest <ssk5442@chungbuk.ac.kr>
2023-04-06 22:56:53 +0900

    padding explain code added

--- padding.c
+++ padding.c
@@ -9,9 +9,12 @@ struct student {
 int main() {
     struct student pst;
         
+    // 구조체 student의 크기 출력
     printf("size of student = %ld\n", sizeof(struct student)); 
+    // 정수형의 크기 출력
     printf("size of int = %ld\n", sizeof(int)); 
+    // short 크기 출력
     printf("size of short = %ld\n", sizeof(short));
-    
+
     return 0; 
 }
