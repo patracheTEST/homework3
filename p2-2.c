d39e5403e7986a7f3f122c8d882d800d72900c77
patracheTest <ssk5442@chungbuk.ac.kr>
2023-04-06 22:53:30 +0900

    struct explain added

--- p2-2.c
+++ p2-2.c
@@ -8,6 +8,7 @@ int main() {
 
     int one[] = {0, 1, 2, 3, 4};
 
+    // one이라는 배열의 위치 출력
     printf("one     = %p\n", one);
     printf("&one    = %p\n", &one);
     printf("&one[0] = %p\n", &one[0]);
@@ -15,17 +16,19 @@ int main() {
     printf("------------------------------------\n"); 
     printf(" print_one(&one[0], 5) \n"); 
     printf("------------------------------------\n"); 
+    // 시작위치로 &one[0] 을 준 값에 대하여 각 위치와 값 출력
     print_one(&one[0], 5);
     printf("------------------------------------\n");
     printf(" print_one(one, 5) \n");
     printf("------------------------------------\n"); 
+    // 시작위치로 one 을 준 값에 대하여 각 위치와 값 출력
     print_one(one, 5);
 
     return 0; 
 }
 
-void print_one(int *ptr, int rows)
-{/* print out a one-dimensional array using a pointer */
+void print_one(int *ptr, int rows) {
+    /* print out a one-dimensional array using a pointer */
     int i;
 
     printf ("Address \t Contents\n"); 
--- struct.c
+++ struct.c
@@ -18,25 +18,31 @@ int main() {
 
     printf("[----- [김상수] [2018038078] -----]")
 
+    // student1을 이용해서 st1 생성
     struct student1 st1 = {'A', 100, 'A'};
 
+    // st1 의 값 출력
     printf("st1.lastName = %c\n", st1.lastName); 
     printf("st1.studentId = %d\n", st1.studentId); 
     printf("st1.grade = %c\n", st1.grade);
 
+    // student2를 이용해서 st2 생성
     student2 st2 = {'B', 200, 'B'};
 
+    // st2 의 값 출력
     printf("\nst2.lastName = %c\n", st2.lastName); 
     printf("st2.studentId = %d\n", st2.studentId); 
     printf("st2.grade = %c\n", st2.grade);
 
     student2 st3;
+    // st3에 st2 값 대입
     st3 = st2;
 
+    // st3 값 출력
     printf("\nst3.lastName = %c\n", st3.lastName); 
     printf("st3.studentId = %d\n", st3.studentId); 
     printf("st3.grade = %c\n", st3.grade);
-    
+
     /* equality test */
     if(compareStudent(st2, st3)) /* not working */
         printf("equal\n"); 
@@ -45,6 +51,8 @@ int main() {
     return 0; 
 }
 
+// student2의 값을 하나하나 비교하는 함수 출력
+// 맞으면 1, 아니면 0 반환
 int compareStudent(student2 st1, student2 st2){
     if(
         st1.grade == st2.grade &&
