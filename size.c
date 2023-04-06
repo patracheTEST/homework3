56c429fe9eb5d5c34a797ec569b187bfce9eb187
patracheTest <ssk5442@chungbuk.ac.kr>
2023-04-06 22:55:13 +0900

    size explain code added

--- size.c
+++ size.c
@@ -4,10 +4,13 @@
 int main(){
 
     printf("[----- [김상수] [2018038078] -----]")
-
+    // 이중 포인터 x 선언
     int **x;
 
+    // x의 크기 출력 : 4 (int형)
     printf("sizeof(x) = %lu\n", sizeof(x));
+    // 포인터 x의 크기 출력 : 8 (포인터)
     printf("sizeof(*x) = %lu\n", sizeof(*x)); 
+    // 이중 포인터 x의 크기 출력 : 8 (포인터)
     printf("sizeof(**x) = %lu\n", sizeof(**x));
 }
